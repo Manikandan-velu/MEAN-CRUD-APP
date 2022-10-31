086a7da55aee3465754762db2015a287170743bf
ManikandanVelu <manikandan.velu@ihorizons.com>
2022-10-31 08:08:35 +0300

    first-commit-mean-app

---
+++ backend/models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const uniqueValidatore = require("mongoose-unique-validator");
+
+const userSchema = mongoose.Schema({
+  email:{type:String, required:true, unique:true},
+  password:{type:String, required:true}
+});
+
+userSchema.plugin(uniqueValidatore);
+
+
+module.exports = mongoose.model("User", userSchema);
