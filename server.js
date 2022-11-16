40fc53879d95d0951d0bbc6ca8e41455b14fb4e3
ManikandanVelu <manikandan.velu@ihorizons.com>
2022-11-16 09:10:22 +0300

    updated-creating-post

--- server.js
+++ server.js
@@ -20,27 +20,27 @@ const normalizePort = val => {
 
 const onError = error => {
   if (error.syscall !== "listen") {
-    throw error;
+  	throw error;
   }
   const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
   switch (error.code) {
     case "EACCES":
-      console.error(bind + " requires elevated privileges");
-      process.exit(1);
-      break;
+    	console.error(bind + " requires elevated privileges");
+    	process.exit(1);
+    	break;
     case "EADDRINUSE":
-      console.error(bind + " is already in use");
-      process.exit(1);
-      break;
+    	console.error(bind + " is already in use");
+    	process.exit(1);
+    	break;
     default:
-      throw error;
+    	throw error;
   }
 };
 
 const onListening = () => {
-  const addr = server.address();
-  const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
-  debug("Listening on " + bind);
+	const addr = server.address();
+	const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
+	debug("Listening on " + bind);
 };
 
 const port = normalizePort(process.env.PORT || "3000");
